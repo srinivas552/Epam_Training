57739d33cbeb2f26ae7c3b6133666413d6fdc2ad
Srinivas Chintakindhi <Srinivas_Chintakindhi@epam.com>
2018-01-12 12:05:00 +0530

    this is FirstWeekFirstDay Task first commit

---
+++ FirstWeekFirstDay.java
@@ -0,0 +1,18 @@
+package com.epam.rd.weekone;
+/**
+* This code will print "Hello RD" 
+* to the console
+*/
+
+/*
+* Public class is FirstWeekFirstDay same as Filenname 
+* which gives "Hello RD" to the console
+*/
+
+public class FirstWeekFirstDay {
+    // Main method to be called by JVM
+    public static void main(String args[]) {
+	// Printing output
+	System.out.println("Hello RD");
+    }
+}
